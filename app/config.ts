965ff071a64a7bd7a1e2e558fbf761420b619d9b
litoma <litoma4u@gmail.com>
2025-06-14 12:53:31 +0000

    Customize Nextfolio

---
+++ app/config.ts
@@ -0,0 +1,12 @@
+export const metaData = {
+  baseUrl: "https://yusukesakai.com", // カスタムドメインに変更
+  title: "Yusuke Sakai's Portfolio", // 任意のタイトル
+  name: "Yusuke Sakai", // あなたの名前
+  ogImage: "/opengraph-image.png", // OG画像パス（必要に応じて更新）
+  description: "Yusuke Sakai's personal portfolio built with Next.js.", // 説明
+};
+export const socialLinks = {
+  twitter: "https://x.com/litoma", // あなたのTwitter URL
+  github: "https://github.com/litoma", // あなたのGitHub URL
+  email: "mailto:mail@yusukesakai.com", // あなたのメール
+};

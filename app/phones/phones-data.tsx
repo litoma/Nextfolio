a384bba692c6959d9c09f762465371a92b789423
litoma <litoma4u@gmail.com>
2025-06-17 13:25:10 +0000

    Update phones page

--- app/phones/phones-data.tsx
+++ app/phones/phones-data.tsx
@@ -1,21 +1,63 @@
 export interface Phone {
   title: string;
-  year: number;
+  year: string;
   description: string;
   url: string;
 }
 
 export const phones: Phone[] = [
   {
-    title: "Mithril AI",
-    year: 2024,
-    description: "Open science AI resarch lab",
-    url: "https://github.com/mithrilai",
+    title: "Google Pixel 7 Pro",
+    year: "2022-",
+    description: "現在使用中。",
+    url: "https://k-tai.watch.impress.co.jp/docs/column/stapa/1458586.html",
   },
   {
-    title: "OpenDeepLearning",
-    year: 2023,
-    description: "Open source AI education resources",
-    url: "https://opendeeplearning.xyz/",
+    title: "HUAWEI Mate 20 Pro",
+    year: "2018-2022",
+    description: "発売初日に購入。満足度高い。",
+    url: "https://k-tai.watch.impress.co.jp/topics/huawei-mate20pro1811/",
+  },
+  {
+    title: "Google Nexus 5",
+    year: "2014-2018",
+    description: "同一機種を3台。",
+    url: "https://k-tai.watch.impress.co.jp/docs/review/623563.html",
+  },
+  {
+    title: "Xperia AX SO-01E",
+    year: "2012-2014",
+    description: "背面が湾曲していて持ちやすい。",
+    url: "https://k-tai.watch.impress.co.jp/docs/showcase/573690.html",
+  },
+  {
+    title: "GALAXY S SC-02B",
+    year: "2010-2012",
+    description: "初のスマホ。",
+    url: "https://k-tai.watch.impress.co.jp/docs/review/402703.html",
+  },
+  {
+    title: "FOMA D905i",
+    year: "2008-2010",
+    description: "クルクルが最強に便利。",
+    url: "https://k-tai.watch.impress.co.jp/cda/article/showcase_top/37545.html",
+  },
+  {
+    title: "FOMA SO902i",
+    year: "2006-2008",
+    description: "大学進学とともに購入。",
+    url: "https://k-tai.watch.impress.co.jp/cda/article/showcase_top/28337.html",
+  },
+  {
+    title: "Vodafone V602SH",
+    year: "2004-2006",
+    description: "自分で初めて選んだケータイ。",
+    url: "https://k-tai.watch.impress.co.jp/cda/article/showcase_top/19661.html",
+  },
+  {
+    title: "J-PHONE J-N04",
+    year: "2002-2004",
+    description: "高校進学とあわせて購入してもらった初めてのケータイ。",
+    url: "https://k-tai.watch.impress.co.jp/cda/article/showcase_top/7639.html",
   },
 ];
